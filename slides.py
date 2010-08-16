f77b52c9ce638a1bab7c3db4c98bf55b63306b8c
Pamela Fox <pamelafox@gmail.com>
2010-08-16 00:27:37 +0000

    Adding 5lide-maker

---
+++ slides.py
@@ -0,0 +1,418 @@
+#!/usr/bin/env python
+#
+# Copyright 2008 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+"""A collaborative slide making web application built on Google App Engine.
+   Based on tasks list app by Bret Taylor."""
+
+import datetime
+import os
+import random
+import string
+import sys
+
+from google.appengine.api import users
+from google.appengine.ext import db
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp import template
+from google.appengine.ext.webapp.util import login_required
+from google.appengine.ext.webapp.util import run_wsgi_app
+
+
+# Set to true if we want to have our webapp print stack traces, etc
+_DEBUG = True
+
+
+# Add our custom Django template filters to the built in filters
+template.register_template_library('templatefilters')
+
+
+class SlideSet(db.Model):
+  """A SlideSet is that encompasses all the slides.
+
+  Other than the tasks referring to it, a SlideSet just has meta-data, like
+  whether it is published and the date at which it was last updated.
+  """
+  name = db.StringProperty(required=True)
+  created = db.DateTimeProperty(auto_now_add=True)
+  updated = db.DateTimeProperty(auto_now=True)
+  published = db.BooleanProperty(default=False)
+  theme = db.StringProperty()
+  
+  @staticmethod
+  def get_current_user_sets():
+    """Returns the slidesets that the current user has access to."""
+    return SlideSet.get_user_sets(users.get_current_user())
+
+  @staticmethod
+  def get_user_sets(user):
+    """Returns the slide sets that the given user has access to."""
+    if not user: return []
+    memberships = db.Query(SlideSetMember).filter('user =', user)
+    return [m.slide_set for m in memberships]
+
+  def current_user_has_access(self):
+    """Returns true if the current user has access to this slide set."""
+    return self.user_has_access(users.get_current_user())
+
+  def user_has_access(self, user):
+    """Returns true if the given user has access to this slide set."""
+    if not user: return False
+    query = db.Query(SlideSetMember)
+    query.filter('slide_set =', self)
+    query.filter('user =', user)
+    return query.get()
+
+
+class SlideSetMember(db.Model):
+  """Represents the many-to-many relationship between SlideSets and Users.
+
+  This is essentially the slide set Access Control List (ACL).
+  """
+  slide_set = db.Reference(SlideSet, required=True)
+  user = db.UserProperty(required=True)
+
+
+class Slide(db.Model):
+  """Represents a single slide in a slide set.
+
+  A slide can be one of several types with different sets of content
+    Intro: Title, SubTitle, Section
+    Section: Title, Section
+    Default: Title, Content, Section
+   
+  """
+  
+  TYPE_INTRO = 'intro'
+  TYPE_SECTION = 'section'
+  TYPE_NORMAL = 'normal'
+  
+  type = db.StringProperty(required=True)
+  title = db.StringProperty()
+  subtitle = db.StringProperty()
+  section = db.StringProperty()
+  content = db.TextProperty()
+  index = db.IntegerProperty()
+  slide_set = db.Reference(SlideSet)
+  created = db.DateTimeProperty(auto_now_add=True)
+  updated = db.DateTimeProperty(auto_now=True)
+
+
+class BaseRequestHandler(webapp.RequestHandler):
+  """Supplies a common template generation function.
+
+  When you call generate(), we augment the template variables supplied with
+  the current user in the 'user' variable and the current webapp request
+  in the 'request' variable.
+  """
+  def generate(self, template_name, template_values={}):
+    values = {
+        'request': self.request,
+        'user': users.get_current_user(),
+        'login_url': users.create_login_url(self.request.uri),
+        'logout_url': users.create_logout_url('http://%s/' % (
+            self.request.host,)),
+        'debug': True,
+        'application_name': 'Task Manager',}
+    values.update(template_values)
+    directory = os.path.dirname(__file__)
+    path = os.path.join(directory, os.path.join('templates', template_name))
+    self.response.out.write(template.render(path, values, debug=_DEBUG))
+
+
+class InboxPage(BaseRequestHandler):
+  """Lists all the slide sets for the current user."""
+  @login_required
+  def get(self):
+    sets = SlideSet.get_current_user_sets()
+    self.generate('index.html', {
+        'sets': sets})
+
+
+class SlideSetPage(BaseRequestHandler):
+  """Displays a single slide set based on ID.
+
+  If the slide set is not published, we give a 403 unless the user is a
+  collaborator on the list. If it is published, but the user is not a
+  collaborator, we show the more limited HTML view of the slide set rather
+  than the interactive AJAXy edit page.
+  """
+
+  # The different slide set output types we support: content types and
+  # template file extensions
+  _OUTPUT_TYPES = {
+    'edit': ['text/html', 'html'],
+    'slide': ['text/html', 'html'],
+    'atom': ['application/atom+xml', 'xml'],}
+
+  def get(self):
+    slide_set = SlideSet.get(self.request.get('id'))
+    if not slide_set:
+      self.error(403)
+      return
+
+    # Choose a template based on the output type
+    output_name = self.request.get('output')
+    if output_name not in SlideSetPage._OUTPUT_TYPES:
+      output_name = 'edit'
+    output_type = SlideSetPage._OUTPUT_TYPES[output_name]
+
+    # Validate this user has access to this slide set. If not, they can
+    # access the html view of this list only if it is published.
+    if not slide_set.current_user_has_access():
+      if slide_set.published:
+        if output_name == 'edit':
+          output_name = 'slide'
+          output_type = SlideSetPage._OUTPUT_TYPES[output_name]
+      else:
+        user = users.get_current_user()
+        if not user:
+          self.redirect(users.create_login_url(self.request.uri))
+        else:
+          self.error(403)
+        return
+
+    # Filter out archived tasks by default
+    slides = list(slide_set.slide_set.order('-index').order('created'))
+
+    self.response.headers['Content-Type'] = output_type[0]
+    self.generate('slideset_%s.%s' % (output_name, output_type[1]), {
+        'slide_set': slide_set,
+        'slides': slides
+        })
+
+
+class CreateSlideSetAction(BaseRequestHandler):
+  """Creates a new slide set for the current user."""
+  def post(self):
+    user = users.get_current_user()
+    name = self.request.get('name')
+    if not user or not name:
+      self.error(403)
+      return
+
+    slide_set = SlideSet(name=name)
+    slide_set.put()
+    slide_set_member = SlideSetMember(slide_set=slide_set, user=user)
+    slide_set_member.put()
+
+    if self.request.get('next'):
+      self.redirect(self.request.get('next'))
+    else:
+      self.redirect('/list?id=' + str(slide_set.key()))
+
+
+class EditSlideAction(BaseRequestHandler):
+  """Edits a specific task, changing its description.
+
+  We also updated the last modified date of the slide set so that the
+  slide set inbox shows the correct last modified date for the list.
+
+  This can be used in an AJAX way or in a form. In a form, you should
+  supply a "next" argument that denotes the URL we should redirect to
+  after the edit is complete.
+  """
+  def post(self):
+    title = self.request.get('title')
+    if not title:
+      self.error(403)
+      return
+    type = self.request.get('type')
+    subtitle = self.request.get('subtitle')
+    content = self.request.get('content')
+    
+    # Get the existing slide that we are editing
+    slide_key = self.request.get('slide')
+    if slide_key:
+      slide = Slide.get(slide_key)
+      if not slide:
+        self.error(403)
+        return
+      slide_set = slide.slide_set
+    else:
+      slide = None
+      slide_set = SlideSet.get(self.request.get('set'))
+
+    # Validate this user has access to this slide set
+    if not slide_set or not slide_set.current_user_has_access():
+      self.error(403)
+      return
+
+    # Create the slide
+    if slide:
+      slide.title = title
+      slide.subtitle = subtitle
+      slide.content = content
+    else:
+      slide = Slide(type=type, title=title, slide_set=slide_set, content=content, subtitle=subtitle)
+    slide.put()
+
+    # Update the slide set so it's updated date is updated. Saving it is all
+    # we need to do since that field has auto_now=True
+    slide_set.put()
+
+    # Only redirect if "next" is given
+    next = self.request.get('next')
+    if next:
+      self.redirect(next)
+    else:
+      self.response.headers['Content-Type'] = 'text/plain'
+      self.response.out.write(str(slide.key()))
+
+
+class AddMemberAction(BaseRequestHandler):
+  """Adds a new User to a SlideSet ACL."""
+  def post(self):
+    slide_set = SlideSet.get(self.request.get('list'))
+    email = self.request.get('email')
+    if not slide_set or not email:
+      self.error(403)
+      return
+
+    # Validate this user has access to this slide set
+    if not slide_set.current_user_has_access():
+      self.error(403)
+      return
+
+    # Don't duplicate entries in the permissions datastore
+    user = users.User(email)
+    if not slide_set.user_has_access(user):
+      member = SlideSetMember(user=user, slide_set=slide_set)
+      member.put()
+    self.redirect(self.request.get('next'))
+
+
+class InboxAction(BaseRequestHandler):
+  """Performs an action in the user's SlideSet inbox.
+
+  We support Archive, Unarchive, and Delete actions. The action is specified
+  by the "action" argument in the POST. The names are capitalized because
+  they correspond to the text in the buttons in the form, which all have the
+  name "action".
+  """
+  def post(self):
+    action = self.request.get('action')
+    lists = self.request.get('list', allow_multiple=True)
+    if not action in ['Delete']:
+      self.error(403)
+      return
+
+    for key in lists:
+      slide_set = SlideSet.get(key)
+
+      # Validate this user has access to this slide set
+      if not slide_set or not slide_set.current_user_has_access():
+        self.error(403)
+        return
+        
+        
+      for member in slide_set.SlideSetmember_set:
+        member.delete()
+      for slide in slide_set.slide_set:
+        slide.delete()
+      slide_set.delete()
+
+    self.redirect(self.request.get('next'))
+
+
+class SlideSetAction(BaseRequestHandler):
+  """Performs an action on a specific slide set.
+  by the "action" argument in the POST.
+  """
+  def post(self):
+    action = self.request.get('action')
+    slides = self.request.get('slide', allow_multiple=True)
+    if not action in ['Delete']:
+      self.error(403)
+      return
+
+    for key in slides:
+      slide = Slide.get(key)
+
+      # Validate this user has access to this slide set
+      if not slide or not slide.slide_set.current_user_has_access():
+        self.error(403)
+        return
+
+      slide.delete()
+
+    self.redirect(self.request.get('next'))
+
+
+class SetSlidePositionsAction(BaseRequestHandler):
+  """Orders the slides in a slide sets.
+
+  The input to this handler is a comma-separated list of slide keys in the
+  "slides" argument to the post. We assign priorities to the given slides
+  based on that order (e.g., 1 through N for N slides).
+  """
+  def post(self):
+    keys = self.request.get('slides').split(',')
+    if not keys:
+      self.error(403)
+      return
+    num_keys = len(keys)
+    for i, key in enumerate(keys):
+      key = keys[i]
+      slide = Slide.get(key)
+      if not slide or not slide.slide_set.current_user_has_access():
+        self.error(403)
+        return
+      slide.index = num_keys - i - 1
+      slide.put()
+
+
+class PublishSlideSetAction(BaseRequestHandler):
+  """Publishes a given slide set, which makes it viewable by everybody."""
+  def post(self):
+    slide_set = SlideSet.get(self.request.get('id'))
+    if not slide_set or not slide_set.current_user_has_access():
+      self.error(403)
+      return
+
+    slide_set.published = bool(self.request.get('publish'))
+    slide_set.put()
+    
+class ChangeThemeAction(BaseRequestHandler):
+  """Publishes a given slide set, which makes it viewable by everybody."""
+  
+  def post(self):
+    slide_set = SlideSet.get(self.request.get('id'))
+    if not slide_set or not slide_set.current_user_has_access():
+      self.error(403)
+      return
+
+    slide_set.theme = self.request.get('theme')
+    slide_set.put()
+
+
+def main():
+  application = webapp.WSGIApplication([
+      ('/', InboxPage),
+      ('/list', SlideSetPage),
+      ('/editslide.do', EditSlideAction),
+      ('/createslideset.do', CreateSlideSetAction),
+      ('/addmember.do', AddMemberAction),
+      ('/inboxaction.do', InboxAction),
+      ('/slideset.do', SlideSetAction),
+      ('/publishslideset.do', PublishSlideSetAction),
+      ('/changetheme.do', ChangeThemeAction),
+      ('/setslidepositions.do', SetSlidePositionsAction)], debug=_DEBUG)
+  run_wsgi_app(application)
+
+
+if __name__ == '__main__':
+  main()
---
+++ static/javascript/debug/slideset.js
@@ -0,0 +1,307 @@
+// Copyright 2007 Google Inc.
+// All Rights Reserved
+//
+// Author: Bret Taylor
+
+// A single slide. We know how to draw ourselves in the DOM and support the
+// dragging/reordering operations on slides.
+function Slide(setKey, key, type, title, subtitle, content) {
+  this.setKey_ = setKey;
+  this.key_ = key;
+  this.type_ = type; 
+  this.title_ = title;
+  this.subtitle_ = subtitle || '';
+  this.content_ = content || '';
+}
+
+Slide.TYPE_INTRO = 'intro';
+Slide.TYPE_SECTION = 'section';
+Slide.TYPE_NORMAL = 'normal';
+
+// Returns this slide's datastore key.
+Slide.prototype.key = function() {
+  return this.key_;
+}
+
+// Parses a list of slides of the form:
+//
+//   [[setKey, key, description, completed]*]
+//
+// Specifically, the list should be an array of arrays representing arguments
+// to the Slide constructor.
+Slide.parseList = function(list) {
+  var result = [];
+  function wrapper() {}
+  wrapper.prototype = Slide.prototype;
+  for (var i = 0; i < list.length; i++) {
+    var slide = new wrapper();
+    Slide.apply(slide, list[i]);
+    result.push(slide);
+  }
+  return result;
+}
+
+// Attaches this slide to the given element container, creating the DOM
+// elements representing this slide.
+Slide.prototype.attach = function(container) {
+  var element = this.createElement_("div", container, "slide");
+  element.style.position = "relative";
+
+  var table = this.createElement_("table", element);
+  var tbody = this.createElement_("tbody", table);
+  var tr = this.createElement_("tr", tbody);
+  
+  var typeCell = this.createElement_("td", tr, "typecol");
+  var typeImg = this.createElement_("img", typeCell);
+  typeImg.src = "/static/images/type_" + this.type_ + ".png";
+  
+  var titleCell = this.createElement_("td", tr, "titlecol");
+  this.titleContainer_ = this.createElement_("input", titleCell);
+  this.titleContainer_.style.position = "relative";
+  this.titleContainer_.value = this.title_ || "";
+  Event.addListener(this.titleContainer_, "keypress", callback(this, this.onEditKeyPress_));
+  Event.addListener(this.titleContainer_, "blur", callback(this, this.saveEdit_));
+  Event.addListener(this.titleContainer_, "mousedown", stopEvent);
+  
+   // Make subtitle cell
+  var subtitleCell = this.createElement_("td", tr, "subtitlecol");
+  this.subtitleContainer_ = this.createElement_("input", subtitleCell);
+  if (this.type_ == Slide.TYPE_NORMAL) this.subtitleContainer_.disabled = true;
+  Event.addListener(this.subtitleContainer_, "keypress", callback(this, this.onEditKeyPress_));
+  Event.addListener(this.subtitleContainer_, "blur", callback(this, this.saveEdit_));
+  Event.addListener(this.subtitleContainer_, "mousedown", stopEvent);
+  this.subtitleContainer_.value = this.subtitle_ || "";
+  
+  // Make content input
+  var contentCell = this.createElement_("td", tr, "contentcol");
+  this.contentContainer_ = this.createElement_("textarea", contentCell);
+  this.contentContainer_.style.width = "60%";
+  if (this.type_ != Slide.TYPE_NORMAL) this.contentContainer_.disabled = true;
+  Event.addListener(this.contentContainer_, "keypress", callback(this, this.onEditKeyPress_));
+  Event.addListener(this.contentContainer_, "blur", callback(this, this.saveEdit_));
+  Event.addListener(this.contentContainer_, "mousedown", stopEvent);
+  this.contentContainer_.value = this.content_ || "";
+  
+
+  // Enable drag positioning of this slide
+  var dragger = new Dragger(element, true);
+  Event.addListener(dragger, "dragstart", callback(this, this.onDragStart_));
+  Event.addListener(dragger, "dragend", callback(this, this.onDragEnd_));
+  Event.addListener(dragger, "drag", callback(this, this.onDrag_));
+  Event.addListener(dragger, "click", callback(this, this.edit));
+
+  this.element_ = element;
+  return element;
+}
+
+// Check for Return/Escape key press events to stop editing. We save the
+// edit on Return, cancel the edit on escape.
+Slide.prototype.onEditKeyPress_ = function(e) {
+  if (e.keyCode == 13) {
+    cancelEvent(e);
+    this.saveEdit_();
+  } else if (e.keyCode == 27) {
+    cancelEvent(e);
+    this.cleanUpEdit_();
+  }
+}
+
+// Save the results of this edit to the server.
+Slide.prototype.saveEdit_ = function() {
+  var titleChanged = this.titleContainer_.value != this.title_;
+  if (titleChanged) this.title_ = this.titleContainer_.value;
+  var subtitleChanged = this.subtitleContainer_ && this.subtitleContainer_.value != this.subtitle_;
+  if (subtitleChanged) this.subtitle_ = this.subtitleContainer_.value;
+  var contentChanged = this.contentContainer_ && this.contentContainer_.value != this.content_;
+  if (contentChanged) this.content_ = this.contentContainer_.value;
+  var changed = contentChanged || subtitleChanged || titleChanged;
+  
+  if (changed) {
+    this.save();
+  }
+}
+
+// Saves this slide to the datastore on the server with an AJAX request.
+Slide.prototype.save = function() {
+  var args = [
+      "set=" + encodeURIComponent(this.setKey_),
+      "type=" + encodeURIComponent(this.type_),
+      "title=" + encodeURIComponent(this.title_),
+      "subtitle=" + encodeURIComponent(this.subtitle_),
+      "content=" + encodeURIComponent(this.content_)
+  ];
+  if (this.key_) {
+    args.push("slide=" + encodeURIComponent(this.key_));
+  }
+  download("/editslide.do", callback(this, this.onSave_), {
+    post: true,
+    body: args.join("&")
+  });
+}
+
+// Called when the save slide AJAX request finishes.
+Slide.prototype.onSave_ = function(text, status) {
+  if (status >= 200 && status < 300) {
+    this.key_ = text;
+  }
+}
+
+// Let the user drag this slide up and down. We remove our element from the
+// DOM and replace it with a placeholder so the user can see where the
+// slide will snap into place when it is dropped.
+Slide.prototype.onDragStart_ = function() {
+  var placeholder = document.createElement("div");
+  placeholder.style.width = this.element_.offsetWidth + "px";
+  placeholder.style.height = this.element_.offsetHeight + "px";
+  this.element_.parentNode.insertBefore(placeholder, this.element_);
+  setOpacity(this.element_, 0.5);
+  this.placeholder_ = placeholder;
+}
+
+// Reposition our placeholder based on the current position of the slide
+// being dragged.
+Slide.prototype.onDrag_ = function() {
+  var container = this.element_.parentNode;
+  var top = this.element_.offsetTop;
+  var bottom = this.element_.offsetTop + this.element_.offsetHeight;
+
+  for (var sibling = container.firstChild; sibling != null;
+       sibling = sibling.nextSibling) {
+    if (sibling == this.element_ || sibling == this.placeholder_) continue;
+
+    var siblingTop = sibling.offsetTop;
+    var siblingBottom = sibling.offsetTop + sibling.offsetHeight;
+    var siblingMiddle = (siblingTop + siblingBottom) / 2;
+
+    if (siblingTop > bottom) continue;
+    if (siblingBottom < top) continue;
+
+    if (siblingTop < top && top < siblingMiddle) {
+      if (this.placeholder_.nextChild != sibling) {
+        container.removeChild(this.placeholder_);
+        container.insertBefore(this.placeholder_, sibling);
+        return;
+      }
+    }
+
+    if (bottom > siblingMiddle) {
+      if (sibling.nextChild != this.placeholder_) {
+        container.removeChild(this.placeholder_);
+        container.insertBefore(this.placeholder_, sibling.nextSibling);
+        return;
+      }
+    }
+  }
+}
+
+// Place our slide back into the slide list DOM and get rid of the placeholder.
+Slide.prototype.onDragEnd_ = function() {
+  if (!this.placeholder_) return;
+  var container = this.element_.parentNode;
+  container.removeChild(this.element_);
+  this.element_.style.position = "relative";
+  this.element_.style.width = "auto";
+  this.element_.style.height = "auto";
+  this.element_.style.left = "auto";
+  this.element_.style.top = "auto";
+  setOpacity(this.element_, 1);
+  container.insertBefore(this.element_, this.placeholder_);
+  container.removeChild(this.placeholder_);
+  this.placeholder_ = null;
+  Event.trigger(this, "positionchanged");
+}
+
+// Creates a DOM element with the given name, parent, and class name.
+Slide.prototype.createElement_ = function(name, opt_parent, opt_className) {
+  var element = document.createElement(name);
+  if (opt_className) {
+    element.className = opt_className;
+  }
+  if (opt_parent) {
+    opt_parent.appendChild(element);
+  }
+  return element;
+}
+
+// A slide list, which is just a collection of slides.
+function SlideSet(key, slides) {
+  this.key_ = key;
+  this.slides_ = slides;
+}
+
+// Draws this slide list in the given container.
+SlideSet.prototype.attach = function(container) {
+  var element = document.createElement("div");
+  element.className = "slidelist";
+  element.style.position = "relative";
+  container.appendChild(element);
+  this.element_ = element;
+  var order = [];
+  for (var i = 0; i < this.slides_.length; i++) {
+    var slide = this.slides_[i];
+    order.push(slide.key());
+    var slideElement = slide.attach(element);
+    slideElement.slide = slide;
+    Event.addListener(slide, "positionchanged",
+                      callback(this, this.savePositions_));
+  }
+  this.order_ = order;
+}
+
+// Serializes the order of all of the slides in this list to the server so
+// the order will be preserved on refresh.
+SlideSet.prototype.savePositions_ = function() {
+  // Determine the slide order based on the positions of the DIVs
+  var order = [];
+  for (var child = this.element_.firstChild; child != null;
+       child = child.nextSibling) {
+    if (child.slide) {
+      order.push(child.slide.key());
+    }
+  }
+
+  // Only save the order to the server if it has changed
+  var changed = false;
+  for (var i = 0; i < order.length; i++) {
+    if (order[i] != this.order_[i]) {
+      changed = true;
+      break;
+    }
+  }
+  if (!changed) return;
+  this.order_ = order;
+
+  // Save the order to the server
+  var body = "slides=" + encodeURIComponent(order.join(","));
+  download("/setslidepositions.do", null, {
+    post: true,
+    body: body
+  });
+}
+
+SlideSet.prototype.changeTheme = function(theme) {
+  var body = "theme=" + theme + "&id=" + this.key_;
+  download("/changetheme.do", null, {
+    post: true,
+    body: body
+  });
+}
+
+// Creates a new slide in this list
+SlideSet.prototype.newSlide = function(type) {
+  var slide = new Slide(this.key_, null, type, "", "", "");
+  this.slides_.push(slide);
+  var slideElement = slide.attach(this.element_);
+  slideElement.slide = slide;
+  Event.addListener(slide, "positionchanged",
+                    callback(this, this.savePositions_));
+}
+
+// Export our API symbols
+function exportSymbol(name, symbol) {
+  window[name] = symbol;
+}
+exportSymbol("Slide", Slide);
+exportSymbol("SlideSet", SlideSet);
+exportSymbol("DialogBox", DialogBox);
+exportSymbol("download", download);
