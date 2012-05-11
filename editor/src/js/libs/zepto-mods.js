327ccd232d704ecc5c612e48a6310125668c20dd
Pamela Fox <pamela.fox@gmail.com>
2012-05-11 12:47:52 -0700

    Preliminary backbone port

--- editor/src/js/libs/zepto-mods.js
+++ editor/src/js/libs/zepto-mods.js
@@ -3,6 +3,15 @@
     opacity: true
   };
 
+  var nativeTrim = String.prototype.trim;
+  $.trim = function(str, characters){
+    if (!characters && nativeTrim) {
+      return nativeTrim.call(str);
+    }
+    characters = defaultToWhiteSpace(characters);
+    return str.replace(new RegExp('\^[' + characters + ']+|[' + characters + ']+$', 'g'), '');
+  };
+
   Event.prototype.isDefaultPrevented = function() {
     return this.defaultPrevented;
   };
